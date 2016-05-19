73a3f3c893994d6ea594c8d45a33e768060ddf07
Ehtesham <ehtesham.kafeel@gmail.com>
2016-05-19 20:53:03 +0500

    refactoring gettext calls

--- cms/static/js/views/paging_header.js
+++ cms/static/js/views/paging_header.js
@@ -4,7 +4,7 @@ define([
     'gettext',
     'edx-ui-toolkit/js/utils/html-utils',
     'text!templates/paging-header.underscore'
-], function(_, Backbone, gettext, HtmlUtils, paging_header_template) {
+], function(_, Backbone, gettext, HtmlUtils, pagingHeaderTemplate) {
 
         var PagingHeader = Backbone.View.extend({
             events : {
@@ -29,57 +29,52 @@ define([
                     messageHtml = this.messageHtml(),
                     isNextDisabled = lastPage === 0 || currentPage === lastPage;
 
-                this.$el.html(_.template(paging_header_template)({ messageHtml: messageHtml}));
-                this.$(".previous-page-link")
-                    .toggleClass("is-disabled", currentPage === 1)
+                this.$el.html(_.template(pagingHeaderTemplate)({ messageHtml: messageHtml}));
+                this.$('.previous-page-link')
+                    .toggleClass('is-disabled', currentPage === 1)
                     .attr('aria-disabled', currentPage === 1);
-                this.$(".next-page-link")
-                    .toggleClass("is-disabled", isNextDisabled)
+                this.$('.next-page-link')
+                    .toggleClass('is-disabled', isNextDisabled)
                     .attr('aria-disabled', isNextDisabled);
+
                 return this;
             },
 
             messageHtml: function() {
-                var message = '';
-                var asset_type = false;
+                var message = '',
+                    assetType = false;
+
+                /* jshint -W101 */
                 if (this.view.collection.assetType) {
                     if (this.view.collection.sortDirection === 'asc') {
                         // Translators: sample result:
                         // "Showing 0-9 out of 25 total, filtered by Images, sorted by Date Added ascending"
-                        message = gettext(
-                            'Showing {currentItemRange} out of {totalItemsCount}, filtered by {assetType},' +
-                            ' sorted by {sortName} ascending'
-                        );
+                        message = gettext('Showing {currentItemRange} out of {totalItemsCount}, filtered by {assetType}, sorted by {sortName} ascending');
                     } else {
                         // Translators: sample result:
                         // "Showing 0-9 out of 25 total, filtered by Images, sorted by Date Added descending"
-                        message = gettext(
-                            'Showing {currentItemRange} out of {totalItemsCount}, filtered by {assetType},' +
-                            ' sorted by {sortName} descending');
+                        message = gettext('Showing {currentItemRange} out of {totalItemsCount}, filtered by {assetType}, sorted by {sortName} descending');
                     }
-                    asset_type = this.filterNameLabel();
+                    assetType = this.filterNameLabel();
                 } else {
                     if (this.view.collection.sortDirection === 'asc') {
                         // Translators: sample result:
                         // "Showing 0-9 out of 25 total, sorted by Date Added ascending"
-                        message = gettext(
-                            'Showing {currentItemRange} out of {totalItemsCount}, sorted by {sortName} ascending'
-                        );
+                        message = gettext('Showing {currentItemRange} out of {totalItemsCount}, sorted by {sortName} ascending');
                     } else {
                         // Translators: sample result:
                         // "Showing 0-9 out of 25 total, sorted by Date Added descending"
-                        message = gettext(
-                            'Showing {currentItemRange} out of {totalItemsCount}, sorted by {sortName} descending'
-                        );
+                        message = gettext('Showing {currentItemRange} out of {totalItemsCount}, sorted by {sortName} descending');
                     }
                 }
+                /* jshint +W101 */
 
-                return '<p>' + HtmlUtils.interpolateHtml(message, {
-                        currentItemRange: this.currentItemRangeLabel(),
-                        totalItemsCount: this.totalItemsCountLabel(),
-                        assetType: asset_type,
-                        sortName: this.sortNameLabel()
-                    }) + '</p>';
+                return HtmlUtils.interpolateHtml(message, {
+                    currentItemRange: this.currentItemRangeLabel(),
+                    totalItemsCount: this.totalItemsCountLabel(),
+                    assetType: assetType,
+                    sortName: this.sortNameLabel()
+                });
             },
 
             currentItemRangeLabel: function() {
@@ -87,42 +82,43 @@ define([
                     collection = view.collection,
                     start = (collection.getPageNumber() - 1) * collection.getPageSize(),
                     count = collection.size(),
-                    end = start + count;
-                return HtmlUtils.interpolateHtml(
-                    HtmlUtils.HTML('<span class="count-current-shown">{start}-{end}</span>'), {
-                        start: Math.min(start + 1, end),
-                        end: end
-                    });
+                    end = start + count,
+                    htmlMessage = HtmlUtils.HTML('<span class="count-current-shown">{start}-{end}</span>');
+
+                return HtmlUtils.interpolateHtml(htmlMessage, {
+                    start: Math.min(start + 1, end),
+                    end: end
+                });
             },
 
             totalItemsCountLabel: function() {
-                var totalItemsLabel;
+                var totalItemsLabel,
+                    htmlMessage = HtmlUtils.HTML('<span class="count-total">{totalItemsLabel}</span>');
+
                 // Translators: turns into "25 total" to be used in other sentences, e.g. "Showing 0-9 out of 25 total".
                 totalItemsLabel = HtmlUtils.interpolateHtml(gettext('{totalItems} total'), {
                     totalItems: this.view.collection.getTotalRecords()
                 }, true);
 
-                return HtmlUtils.interpolateHtml(
-                    HtmlUtils.HTML('<span class="count-total">{totalItemsLabel}</span>'), {
-                        totalItemsLabel: totalItemsLabel
-                    }
-                );
+                return HtmlUtils.interpolateHtml(htmlMessage, {
+                    totalItemsLabel: totalItemsLabel
+                });
             },
 
             sortNameLabel: function() {
-                return HtmlUtils.interpolateHtml(
-                    HtmlUtils.HTML('<span class="sort-order">{sortName}</span>'), {
-                        sortName: this.view.sortDisplayName()
-                    }
-                );
+                var htmlMessage = HtmlUtils.HTML('<span class="sort-order">{sortName}</span>');
+
+                return HtmlUtils.interpolateHtml(htmlMessage, {
+                    sortName: this.view.sortDisplayName()
+                });
             },
 
             filterNameLabel: function() {
-                return HtmlUtils.interpolateHtml(
-                    HtmlUtils.HTML('<span class="filter-column">{filterName}</span>'), {
-                        filterName: this.view.filterDisplayName()
-                    }
-                );
+                var htmlMessage = HtmlUtils.HTML('<span class="filter-column">{filterName}</span>');
+
+                return HtmlUtils.interpolateHtml(htmlMessage, {
+                    filterName: this.view.filterDisplayName()
+                });
             },
 
             nextPage: function() {
@@ -135,4 +131,4 @@ define([
         });
 
         return PagingHeader;
-    }); // end define();
+    });
